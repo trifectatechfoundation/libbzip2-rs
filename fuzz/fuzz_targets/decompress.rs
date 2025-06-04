4a018a502788e414048e6095497538b0160d687c
Christian Reitter <78539074+ros-cr@users.noreply.github.com>
2025-06-04 22:20:25 +0200

    Fix CI, avoid false positives in differential testing

--- fuzz/fuzz_targets/decompress.rs
+++ fuzz/fuzz_targets/decompress.rs
@@ -5,12 +5,25 @@ use libfuzzer_sys::fuzz_target;
 // this fuzz target is designed to work directly with unmodified bzip2 files
 fuzz_target!(|fuzz_data: &[u8]| {
     let (err_c, decompressed_c) = unsafe {
-        test_libbz2_rs_sys::decompress_c_with_capacity(1 << 12, fuzz_data.as_ptr(), fuzz_data.len() as _)
+        test_libbz2_rs_sys::decompress_c_with_capacity(
+            1 << 12,
+            fuzz_data.as_ptr(),
+            fuzz_data.len() as _,
+        )
     };
 
     let (err_rs, decompressed_rs) = unsafe {
-        test_libbz2_rs_sys::decompress_rs_with_capacity(1 << 12, fuzz_data.as_ptr(), fuzz_data.len() as _)
+        test_libbz2_rs_sys::decompress_rs_with_capacity(
+            1 << 12,
+            fuzz_data.as_ptr(),
+            fuzz_data.len() as _,
+        )
     };
+
+    #[cfg(feature = "disable-checksum")]
+    compile_error!(
+        "Problematic feature enabled! Skipping CRC checksums will cause false positives here."
+    );
 
     // result codes between the two implementations should be identical
     // previously found https://github.com/trifectatechfoundation/libbzip2-rs/pull/110
