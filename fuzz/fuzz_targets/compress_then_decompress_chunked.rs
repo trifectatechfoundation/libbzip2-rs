ad395b0e33c1bc3aab67f4e42dc2be06deb63e00
Christian Reitter <78539074+ros-cr@users.noreply.github.com>
2025-06-04 14:17:37 +0200

    Rework fuzz tests
    Work done as part of an NLnet Security Evaluation for NLnet NGI Zero Core.
    
    Fix bugs in decompression and output buffer handling, extend differential fuzzing design,
    fuzz more compression parameters, remove now-unnecessary end-to-end test case,
    refactor and clean up code, add extensive code documentation to outline expectations and reasoning,
    rename fuzz tests, rename and disable corpus rejection feature by default,
    add README with fuzz resources, update lockfile.

---
+++ fuzz/fuzz_targets/compress_then_decompress_chunked.rs
@@ -0,0 +1,185 @@
+#![no_main]
+use libbz2_rs_sys::bz_stream;
+use libbz2_rs_sys::BZ2_bzDecompress;
+use libbz2_rs_sys::BZ2_bzDecompressEnd;
+use libbz2_rs_sys::BZ2_bzDecompressInit;
+use libbz2_rs_sys::{
+    BZ_CONFIG_ERROR, BZ_DATA_ERROR, BZ_DATA_ERROR_MAGIC, BZ_FINISH, BZ_FINISH_OK, BZ_FLUSH_OK,
+    BZ_IO_ERROR, BZ_MEM_ERROR, BZ_OK, BZ_OUTBUFF_FULL, BZ_PARAM_ERROR, BZ_RUN_OK,
+    BZ_SEQUENCE_ERROR, BZ_STREAM_END, BZ_UNEXPECTED_EOF,
+};
+
+use libfuzzer_sys::fuzz_target;
+
+fn compress_c(data: &[u8], compression_level: u8, work_factor: u8) -> Vec<u8> {
+    // compress the data with the stock C bzip2
+
+    // output buffer for compression, will get resized later if needed
+    let mut output = vec![0u8; 1024];
+
+    let mut stream = libbz2_rs_sys::bz_stream {
+        next_in: data.as_ptr() as *mut _,
+        avail_in: data.len() as _,
+        total_in_lo32: 0,
+        total_in_hi32: 0,
+        next_out: output.as_mut_ptr() as *mut _,
+        avail_out: output.len() as _,
+        total_out_lo32: 0,
+        total_out_hi32: 0,
+        state: std::ptr::null_mut(),
+        bzalloc: None,
+        bzfree: None,
+        opaque: std::ptr::null_mut(),
+    };
+
+    unsafe {
+        let err = libbz2_rs_sys::BZ2_bzCompressInit(
+            &mut stream,
+            compression_level.into(),
+            0,
+            work_factor.into(),
+        );
+        // init should always succeed
+        assert_eq!(err, BZ_OK);
+    };
+
+    let error = loop {
+        match unsafe { libbz2_rs_sys::BZ2_bzCompress(&mut stream, BZ_FINISH) } {
+            BZ_FINISH_OK => {
+                let used = output.len() - stream.avail_out as usize;
+                // The output buffer is full, resize it
+                let add_space: u32 = Ord::max(1024, output.len().try_into().unwrap());
+                output.resize(output.len() + add_space as usize, 0);
+
+                // If resize() reallocates, it may have moved in memory
+                stream.next_out = output.as_mut_ptr().cast::<i8>().wrapping_add(used);
+                stream.avail_out += add_space;
+
+                continue;
+            }
+            BZ_STREAM_END => {
+                break BZ_OK;
+            }
+            ret => {
+                break ret;
+            }
+        }
+    };
+
+    // compression should always succeed
+    assert_eq!(error, BZ_OK);
+
+    // truncate the output buffer down to the actual number of compressed bytes
+    output.truncate(
+        ((u64::from(stream.total_out_hi32) << 32) + u64::from(stream.total_out_lo32))
+            .try_into()
+            .unwrap(),
+    );
+
+    unsafe {
+        // cleanup, should always succeed
+        let err = libbz2_rs_sys::BZ2_bzCompressEnd(&mut stream);
+        assert_eq!(err, BZ_OK);
+    }
+
+    output
+}
+
+fuzz_target!(|input: (&[u8], usize, u8, u8)| {
+    let (fuzzer_data, chunk_size, compression_decider, work_factor_decider) = input;
+
+    // let the fuzzer pick a value from 1 to 9 (inclusive)
+    // use modulo to ensure this always maps to a valid number
+    let compression_level: u8 = (compression_decider % 9) + 1;
+
+    // valid values 0 to 250 (inclusive)
+    // use modulo to ensure this always maps to a valid number
+    let work_factor = work_factor_decider % 251;
+
+    if chunk_size == 0 {
+        // we can't iterate over chunks of size 0 byte, exit early
+        return;
+    }
+
+    let compressed_data = compress_c(fuzzer_data, compression_level, work_factor);
+
+    let mut stream = bz_stream::zeroed();
+
+    unsafe {
+        let err = BZ2_bzDecompressInit(&mut stream, 0, 0);
+        assert_eq!(err, BZ_OK);
+    };
+
+    // output buffer for decompression, will get resized later if needed
+    let mut output = vec![0u8; 1 << 10];
+    stream.next_out = output.as_mut_ptr() as *mut _;
+    stream.avail_out = output.len() as _;
+
+    // iterate over chunks of the compressed data
+    'decompression: for chunk in compressed_data.as_slice().chunks(chunk_size) {
+        // designate the current chunk as input
+        stream.next_in = chunk.as_ptr() as *mut _;
+        stream.avail_in = chunk.len() as _;
+
+        loop {
+            // perform one round of decompression
+            let err = unsafe { BZ2_bzDecompress(&mut stream) };
+            match err {
+                BZ_OK => {
+                    // the decompression mechanism still has data in the input buffer,
+                    // but no more space in the output buffer
+                    if stream.avail_in > 0 && stream.avail_out == 0 {
+                        let used = output.len() - stream.avail_out as usize;
+                        // The dest buffer is full, increase its size
+                        let add_space: u32 = Ord::max(1024, output.len().try_into().unwrap());
+                        output.resize(output.len() + add_space as usize, 0);
+
+                        // If resize() reallocates, it may have moved in memory
+                        stream.next_out = output.as_mut_ptr().cast::<i8>().wrapping_add(used);
+                        stream.avail_out += add_space;
+                        continue;
+                    } else {
+                        // the decompression of this chunk step was successful, move on to the next
+                        break;
+                    }
+                }
+                BZ_STREAM_END => {
+                    // the decompression has completed, exit loops
+                    break 'decompression;
+                }
+                BZ_RUN_OK => panic!("BZ_RUN_OK"),
+                BZ_FLUSH_OK => panic!("BZ_FLUSH_OK"),
+                BZ_FINISH_OK => panic!("BZ_FINISH_OK"),
+                BZ_SEQUENCE_ERROR => panic!("BZ_SEQUENCE_ERROR"),
+                BZ_PARAM_ERROR => panic!("BZ_PARAM_ERROR"),
+                BZ_MEM_ERROR => panic!("BZ_MEM_ERROR"),
+                BZ_DATA_ERROR => {
+                    panic!("BZ_DATA_ERROR")
+                }
+                BZ_DATA_ERROR_MAGIC => panic!("BZ_DATA_ERROR_MAGIC"),
+                BZ_IO_ERROR => panic!("BZ_IO_ERROR"),
+                BZ_UNEXPECTED_EOF => panic!("BZ_UNEXPECTED_EOF"),
+                BZ_OUTBUFF_FULL => panic!("BZ_OUTBUFF_FULL"),
+                BZ_CONFIG_ERROR => panic!("BZ_CONFIG_ERROR"),
+                _ => panic!("{err}"),
+            }
+        }
+    }
+
+    // truncate the output buffer down to the actual number of decompressed bytes
+    output.truncate(
+        ((u64::from(stream.total_out_hi32) << 32) + u64::from(stream.total_out_lo32))
+            .try_into()
+            .unwrap(),
+    );
+
+    unsafe {
+        // cleanup, should always succeed
+        let err = BZ2_bzDecompressEnd(&mut stream);
+        assert_eq!(err, BZ_OK);
+    }
+
+    // round-trip check
+    // compressing and then decompressing should lead back to the input data
+    assert_eq!(output, fuzzer_data);
+});
--- fuzz/fuzz_targets/decompress_chunked.rs
+++ fuzz/fuzz_targets/decompress_chunked.rs
@@ -1,133 +1,128 @@
 #![no_main]
-use libbz2_rs_sys::{BZ_FINISH, BZ_FINISH_OK, BZ_OK, BZ_STREAM_END};
-use libfuzzer_sys::fuzz_target;
 
-fn compress_c(data: &[u8]) -> Vec<u8> {
-    // First, decompress the data with the stock C bzip2.
-    let mut output = vec![0u8; 1024];
+use libbz2_rs_sys::{bz_stream, BZ2_bzDecompress, BZ2_bzDecompressEnd, BZ2_bzDecompressInit};
+use libbz2_rs_sys::{
+    BZ_DATA_ERROR, BZ_DATA_ERROR_MAGIC, BZ_FINISH_OK, BZ_FLUSH_OK, BZ_OK, BZ_OUTBUFF_FULL,
+    BZ_RUN_OK, BZ_STREAM_END,
+};
+use libfuzzer_sys::{fuzz_target, Corpus};
 
-    let mut stream = bzip2_sys::bz_stream {
-        next_in: data.as_ptr() as *mut _,
-        avail_in: data.len() as _,
-        total_in_lo32: 0,
-        total_in_hi32: 0,
-        next_out: output.as_mut_ptr() as *mut _,
-        avail_out: output.len() as _,
-        total_out_lo32: 0,
-        total_out_hi32: 0,
-        state: std::ptr::null_mut(),
-        bzalloc: None,
-        bzfree: None,
-        opaque: std::ptr::null_mut(),
+// this fuzz target is designed to work directly with unmodified bzip2 files
+fuzz_target!(|fuzz_data: &[u8]| -> Corpus {
+    // Limitation: we can't explicitly add extra fuzzer input data to influence the computation
+    // Workaround: pick either small or fast based on a byte of input data
+    let small = fuzz_data
+        .get(fuzz_data.len() / 2)
+        .is_some_and(|v| v % 2 == 0);
+
+    // Small enough to hit interesting cases, but large enough to hit the fast path
+    let chunk_size = 16;
+
+    // Initialize decompression context, this should succeed
+    let mut stream: bz_stream = bz_stream::zeroed();
+    let ret = unsafe { BZ2_bzDecompressInit(&mut stream, 0, small as _) };
+    assert_eq!(ret, libbz2_rs_sys::BZ_OK);
+
+    // libFuzzer has an optional mechanism to explicitly reject fuzz inputs
+    // This directive forces the fuzzer to forget the observed coverage for the current input,
+    // and does not allow adding it to the corpus collection
+    //
+    // See https://llvm.org/docs/LibFuzzer.html#rejecting-unwanted-inputs
+    //
+    // This is a heavy-handed approach and reduces fuzzer coverage visibility into
+    // the rejected code paths, but may be useful for time-constrained runs
+    //
+    // Expected effects:
+    // 1. reduce some runtime overhead on rejected inputs by skipping post-processing steps
+    // 2. increases ratio of "valid" inputs in working corpus
+    //
+    // The expectation is that this makes it more likely to create valid inputs by mutation,
+    // at least on short runs with a limited amount of executions that start on a pre-seeded corpus
+    let invalid_input = if cfg!(feature = "reject-invalid-in-corpus") {
+        // instruct libFuzzer to reject and ignore this input
+        Corpus::Reject
+    } else {
+        // normal neutral behavior
+        Corpus::Keep
     };
 
-    unsafe {
-        let err = bzip2_sys::BZ2_bzCompressInit(&mut stream, 9, 0, 250);
-        assert_eq!(err, BZ_OK);
-    };
+    // initialize output buffer, resizing will happen later if needed
+    // using the input length is just a quick heuristic and not otherwise meaningful or necessary
+    let mut output = vec![0u8; fuzz_data.len()];
+    let output_len = output.len() as _;
 
-    let error = loop {
-        match unsafe { bzip2_sys::BZ2_bzCompress(&mut stream, BZ_FINISH) } {
-            BZ_FINISH_OK => {
-                let used = output.len() - stream.avail_out as usize;
-                // The output buffer is full.
-                let add_space: u32 = Ord::max(1024, output.len().try_into().unwrap());
-                output.resize(output.len() + add_space as usize, 0);
+    // set output buffer in decompression context
+    stream.next_out = output.as_mut_ptr().cast::<core::ffi::c_char>();
+    stream.avail_out = output_len;
 
-                // If resize() reallocates, it may have moved in memory.
-                stream.next_out = output.as_mut_ptr().cast::<i8>().wrapping_add(used);
-                stream.avail_out += add_space;
-
-                continue;
-            }
-            BZ_STREAM_END => {
-                break BZ_OK;
-            }
-            ret => {
-                break ret;
-            }
-        }
-    };
-
-    assert_eq!(error, BZ_OK);
-
-    output.truncate(
-        ((u64::from(stream.total_out_hi32) << 32) + u64::from(stream.total_out_lo32))
-            .try_into()
-            .unwrap(),
-    );
-
-    unsafe {
-        let err = bzip2_sys::BZ2_bzCompressEnd(&mut stream);
-        assert_eq!(err, BZ_OK);
-    }
-
-    output
-}
-
-fuzz_target!(|input: (String, usize)| {
-    use libbz2_rs_sys::*;
-
-    let (data, chunk_size) = input;
-
-    if chunk_size == 0 {
-        return;
-    }
-
-    let deflated = compress_c(data.as_bytes());
-
-    let mut stream = bz_stream::zeroed();
-
-    unsafe {
-        let err = BZ2_bzDecompressInit(&mut stream, 0, 0);
-        assert_eq!(err, BZ_OK);
-    };
-
-    let mut output = vec![0u8; 1 << 15];
-    stream.next_out = output.as_mut_ptr() as *mut _;
-    stream.avail_out = output.len() as _;
-
-    for chunk in deflated.as_slice().chunks(chunk_size) {
-        stream.next_in = chunk.as_ptr() as *mut _;
+    // iterate over chunks in the compressed data
+    'decompression: for chunk in fuzz_data.chunks(chunk_size) {
+        // set new chunk as input
+        stream.next_in = chunk.as_ptr() as *mut core::ffi::c_char;
         stream.avail_in = chunk.len() as _;
 
-        let err = unsafe { BZ2_bzDecompress(&mut stream) };
-        match err {
-            BZ_OK => continue,
-            BZ_RUN_OK => panic!("BZ_RUN_OK"),
-            BZ_FLUSH_OK => panic!("BZ_FLUSH_OK"),
-            BZ_FINISH_OK => panic!("BZ_FINISH_OK"),
-            BZ_STREAM_END => continue,
-            BZ_SEQUENCE_ERROR => panic!("BZ_SEQUENCE_ERROR"),
-            BZ_PARAM_ERROR => panic!("BZ_PARAM_ERROR"),
-            BZ_MEM_ERROR => panic!("BZ_MEM_ERROR"),
-            BZ_DATA_ERROR => panic!("BZ_DATA_ERROR"),
-            BZ_DATA_ERROR_MAGIC => panic!("BZ_DATA_ERROR_MAGIC"),
-            BZ_IO_ERROR => panic!("BZ_IO_ERROR"),
-            BZ_UNEXPECTED_EOF => panic!("BZ_UNEXPECTED_EOF"),
-            BZ_OUTBUFF_FULL => panic!("BZ_OUTBUFF_FULL"),
-            BZ_CONFIG_ERROR => panic!("BZ_CONFIG_ERROR"),
-            _ => panic!("{err}"),
+        // process this chunk
+        loop {
+            // perform decompression
+            match unsafe { BZ2_bzDecompress(&mut stream) } {
+                BZ_STREAM_END => {
+                    // stream complete, stop processing chunks
+                    break 'decompression;
+                }
+                BZ_OK => {
+                    // there is still input from this chunk left to process but no more output buffer
+                    // this means we have to increase the output buffer and retry the decompress
+                    if stream.avail_in > 0 && stream.avail_out == 0 {
+                        let used = output.len() - stream.avail_out as usize;
+                        // The dest buffer is full, resize it
+                        let add_space: u32 = Ord::max(4096, output.len().try_into().unwrap());
+                        output.resize(output.len() + add_space as usize, 0);
+
+                        // If resize() reallocates, it may have moved in memory
+                        stream.next_out = output.as_mut_ptr().cast::<i8>().wrapping_add(used);
+                        stream.avail_out += add_space;
+
+                        continue;
+                    } else if stream.avail_in == 0 {
+                        // stop processing this chunk
+                        break;
+                    }
+
+                    continue;
+                }
+                BZ_DATA_ERROR | BZ_DATA_ERROR_MAGIC => {
+                    // valid reasons for decompression errors if the input is invalid
+
+                    // clean up state
+                    unsafe { BZ2_bzDecompressEnd(&mut stream) };
+
+                    // stop processing
+                    return invalid_input;
+                }
+                BZ_FLUSH_OK => {
+                    panic!("BZ_FLUSH_OK");
+                }
+                BZ_RUN_OK => {
+                    panic!("BZ_RUN_OK");
+                }
+                BZ_FINISH_OK => {
+                    panic!("BZ_FINISH_OK");
+                }
+                BZ_OUTBUFF_FULL => {
+                    panic!("BZ_OUTBUFF_FULL");
+                }
+                err => panic!("{err}"),
+            }
         }
     }
 
-    output.truncate(
-        ((u64::from(stream.total_out_hi32) << 32) + u64::from(stream.total_out_lo32))
-            .try_into()
-            .unwrap(),
-    );
-    let output = String::from_utf8(output).unwrap();
-
     unsafe {
+        // clean up state, should always succeed
         let err = BZ2_bzDecompressEnd(&mut stream);
-        assert_eq!(err, BZ_OK);
+        if err != BZ_OK {
+            return invalid_input;
+        }
     }
 
-    if output != data {
-        let path = std::env::temp_dir().join("deflate.txt");
-        std::fs::write(&path, &data).unwrap();
-        eprintln!("saved input file to {path:?}");
-    }
-
-    assert_eq!(output, data);
+    Corpus::Keep
 });
